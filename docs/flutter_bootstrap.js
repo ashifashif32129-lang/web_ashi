18f47ede916795795e6dd03aadc4b0d3fb78405f
Admin <admin@Admins-MacBook-Air.local>
2026-08-04 14:50:07 +0530

    Final production build with Ashif Saheer details and WhatsApp integration

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"0ed149e63db1f68d2042063baf0a99d969ccd8
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "279750615" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3607731553" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
